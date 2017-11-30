d2b8f93c0148e94e05d50c9477252dbc80523968
gcaetano <giu.caetano@gmail.com>
2017-11-30 07:17:50 +0000

    adapt depth project to the business logic

--- public/app/view/backoffice/security/panel/mobiles/MobilesController.js
+++
@@ -1,5 +0,0 @@
-Ext.define('QT.view.backoffice.security.panel.mobiles.MobilesController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.backoffice-security-panel-mobiles'
-    
-});
--- public/app/view/backoffice/security/panel/reports/ReportsController.js
+++
@@ -1,5 +0,0 @@
-Ext.define('QT.view.backoffice.security.panel.reports.ReportsController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.backoffice-security-panel-reports-reports'
-    
-});
